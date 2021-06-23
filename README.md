77b891a723853768a0aa39107a0e250948fc7ed2
steve <stevetasticsteve@gmail.com>
2021-06-23 16:27:38 +0100

    Initiated repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kovol language tools
+
+Classes and functions for handing kovol language data.
+
+## inlcudes
+- phonemics
+- verb prediction
--- setup.py
+++ setup.py
@@ -9,27 +9,22 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="realpython-reader",
-    version="1.0.0",
-    description="Read the latest Real Python tutorials",
+    name="kovol_language_tools",
+    version="0.0.1",
+    description="Classes and functions for manipulating data in the Kovol langauge of Papua New Guinea.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/realpython/reader",
-    author="Real Python",
-    author_email="info@realpython.com",
-    license="MIT",
+    url="https://github.com/stevetasticsteve/kovol-language-tools",
+    author="Steve Stanley",
+    author_email="stevetasticsteve@gmail.com",
+    license="GNU GPLv3 ",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
     ],
-    packages=["reader"],
+    packages=["kovol_language_tools"],
     include_package_data=True,
-    install_requires=["feedparser", "html2text"],
-    entry_points={
-        "console_scripts": [
-            "realpython=reader.__main__:main",
-        ]
-    },
+    install_requires=["tabulate",],
 )
 
